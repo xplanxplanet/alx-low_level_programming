3c3f56e61c6cce0b77b197c6f56a0bb3e2dcc882
Ezeuko Chimeremeze <ezeukochimeremeze@gmail.com>
2023-06-15 16:18:09 -0700

    changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Entry Point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char piece[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stdout, "%s\n", piece);
+
+	return (1);
+}
