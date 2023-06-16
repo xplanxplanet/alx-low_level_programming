e841307d56a6ba74d2af98bf409da87f7fbbc47f
Ezeuko Chimeremeze <ezeukochimeremeze@gmail.com>
2023-06-16 15:22:31 -0700

    changes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Description: This program uses a while loop to generate
+  *		lowercase alphabets
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char the_alphabets;
+
+	for (the_alphabets = 'z'; the_alphabets >= 'a'; the_alphabets--)
+	{
+		putchar(the_alphabets);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
